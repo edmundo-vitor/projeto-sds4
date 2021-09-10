475e33643bb29c05a318b430745b586037f66ecf
edmundo-vitor <edmundovitor1414@hotmail.com>
2021-09-10 17:28:56 -0300

    pagination

---
+++ backend/src/main/java/com/edmundo/dsvendas/services/SaleService.java
@@ -0,0 +1,30 @@
+package com.edmundo.dsvendas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.edmundo.dsvendas.dto.SaleDTO;
+import com.edmundo.dsvendas.entities.Sale;
+import com.edmundo.dsvendas.repositories.SaleRepository;
+import com.edmundo.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageble) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageble);
+		return result.map(x -> new SaleDTO(x));
+	}
+	
+}
--- backend/src/main/java/com/edmundo/dsvendas/services/SellerService.java
+++ backend/src/main/java/com/edmundo/dsvendas/services/SellerService.java
@@ -1,6 +1,5 @@
 package com.edmundo.dsvendas.services;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
