487edb3762833d95d08262ae32373b77aa4cf83f
edmundo-vitor <edmundovitor1414@hotmail.com>
2021-09-10 20:28:26 -0300

    group by search

--- backend/src/main/java/com/edmundo/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/edmundo/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.edmundo.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.edmundo.dsvendas.dto.SaleDTO;
+import com.edmundo.dsvendas.dto.SaleSuccessDTO;
+import com.edmundo.dsvendas.dto.SaleSumDTO;
 import com.edmundo.dsvendas.entities.Sale;
 import com.edmundo.dsvendas.repositories.SaleRepository;
 import com.edmundo.dsvendas.repositories.SellerRepository;
@@ -27,4 +31,13 @@ public class SaleService {
 		return result.map(x -> new SaleDTO(x));
 	}
 	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amoutGroupedBySeller(){
+		return repository.amoutGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
+	}
 }
