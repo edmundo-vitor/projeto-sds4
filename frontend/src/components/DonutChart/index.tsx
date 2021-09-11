fe588749b44798311befc1a3a51033dc240de297
edmundo-vitor <edmundovitor1414@hotmail.com>
2021-09-11 17:12:57 -0300

    donutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 
 import { SaleSum } from 'types/sale';
@@ -10,24 +11,17 @@ type ChartData = {
 }
 
 const DonutChart = () => {
+   const [chartData, setChartData] = useState<ChartData>( {labels: [], series: []} );
 
-   let chartData: ChartData = {
-      labels: [],
-      series: []
-   }
-
-   axios.get(`${BASE_URL}/salles/amount-by-seller`).then(response => {
-      const data = response.data as SaleSum[];
-      const myLabels = data.map(x => x.sellerName);
-      const mySerires = data.map(x => x.sum);
-
-      chartData = {
-         labels: myLabels,
-         series: mySerires
-      } 
-
-      console.log(chartData);
-   });
+   useEffect(() => {
+      axios.get(`${BASE_URL}/salles/amount-by-seller`).then(response => {
+         const data = response.data as SaleSum[];
+         const myLabels = data.map(x => x.sellerName);
+         const mySerires = data.map(x => x.sum);
+   
+         setChartData({labels: myLabels, series: mySerires}); 
+      });
+   }, []);
 
    const mockData = {
       series: [477138, 499928, 444867, 220426, 473088],
