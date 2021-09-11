8bfd98c26c3aad965b4405259b54c61628572388
edmundo-vitor <edmundovitor1414@hotmail.com>
2021-09-11 18:00:52 -0300

    dataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,27 @@
-export type SaleSum = { 
+import { Seller } from "./seller"
+
+export type Sale = {
+   id: number;
+   visited: number;
+   deals: number;
+   amount: number;
+   date: string;
+   seller: Seller;
+}
+
+export type SalePage = {
+   content?: Sale[];
+   last: boolean;
+   totalPages: number;
+   totalElements: number;
+   first: boolean;
+   size?: number;
+   number: number;
+   numberOfElements?: number;
+   empty?: boolean;
+}
+
+export type SaleSum = {
    sellerName: string;
    sum: number;
 }
@@ -7,4 +30,4 @@ export type SaleSuccess = {
    sellerName: string;
    visited: number;
    deals: number;
- }
+}
